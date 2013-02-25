e47bfdf00783a536a36037a4d4f81ebe54bf90b1
Fernando Gonzalez <ferndogg03@gmail.com>
2013-02-24 19:40:21 -0800

    Better url manipulation

--- background.js
+++ background.js
@@ -1,17 +1,26 @@
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[7].length==11){
+        return match[7];
+    }else{
+        console.debug(url + ": FAILED!!!");
+    }
+}
+
 function directLinks(){
     $("a[href]").each(function () {
         var href = $(this).attr('href');
-        //Try to find/replace short url
-        href = href.replace('youtu.be', 'youtube.com/v');
-        //Try to find/replace regular url
-        href = href.replace('watch?v=', 'v/');
+        var id = youtube_parser(href);
+        if(id){
+            href = "https://youtube.googleapis.com/v/" + id;
+        }
         $(this).attr('href', href);
     });
     console.debug("Links updated.");
 }
 
-//Run script if no other events within 500ms
-//Useful for updating pages that silently update/refresh
+//Run script when page updates
 var timeout = null;
 document.addEventListener("DOMSubtreeModified", function() {
     if(timeout) {
